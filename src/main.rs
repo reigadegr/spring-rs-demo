65bd6c9e6ceda380dad2343e543b165e94364b62
reigadegr <2722688642@qq.com>
2024-09-18 16:04:07 +0800

    集成了一半nacos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use anyhow::Context;
 use spring::{auto_config, App};
 use spring_sqlx::{
@@ -15,16 +16,54 @@ use spring_web::{
 //允许未使用
 #[allow(unused)]
 use spring_web::{get, route};
+use nacos_sdk::api::config::{
+    ConfigChangeListener, ConfigResponse, ConfigService, ConfigServiceBuilder,
+};
+use nacos_sdk::api::constants;
+use nacos_sdk::api::naming::{
+    NamingChangeEvent, NamingEventListener, NamingService, NamingServiceBuilder, ServiceInstance,
+};
+use nacos_sdk::api::props::ClientProps;
 
-// 主函数入口
+struct MyConfigChangeListener;
+
+impl ConfigChangeListener for MyConfigChangeListener  {
+    fn notify(&self, config_resp: ConfigResponse) {
+        tracing::info!("listen the config={}", config_resp);
+    }
+}
+
+pub struct MyNamingEventListener;
+
+impl NamingEventListener for MyNamingEventListener {
+    fn event(&self, event: Arc<NamingChangeEvent>) {
+        tracing::info!("subscriber notify: {:?}", event);
+    }
+}
 #[auto_config(WebConfigurator)]   // 自动扫描web router
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()>{
+    let mut client_props = ClientProps::new();
+    client_props.clone().server_addr("127.0.0.1:8848");
+    client_props.clone().namespace("namespace-id");
+    let config_service = ConfigServiceBuilder::new(client_props.clone())
+        .build();
+
+
+    let naming_service = NamingServiceBuilder::new(client_props)
+        .build();
+    let listener = std::sync::Arc::new(MyNamingEventListener);
+    naming_service.expect("REASON").subscribe("test-service".to_string(),
+                                              Some(constants::DEFAULT_GROUP.to_string()),
+                                              Vec::default(),
+                                              listener);
+
     App::new()
         .add_plugin(SqlxPlugin)  // 添加插件
         .add_plugin(WebPlugin)
         .run()
-        .await
+        .await;
+    Ok(())
 }
 
 // Component可以抽取由Sqlx插件在AppState中的注册的连接池
